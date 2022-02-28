5269b06a617f54e128c54a9a23d7ff9a68b0d0d3
bv@bartosarchitecture.com <bv@bartosarchitecture.com>
2022-02-28 12:38:12 -0800

    Brute Force Approach
    Still more efficent then 100% of submits no TLE hmm

--- 214.ShortestPalindrome.rb
+++ 214.ShortestPalindrome.rb
@@ -4,11 +4,29 @@
 # @param {String} s
 # @return {String}
 
-# Time:
-# Space:
+# Time: O(n^2)
+# Have to iterate over the entire string length
+# Then for each iteration we comapre the subsrings 
+# This is n * n 
+#
+# Space: O(n) or O(1) ?
+# We create a new reverse string to do an initial compare. 
+# I didn't crate any extra strings just used the existsing one to create substrings.  
+#
 # Notes:
-# Approach 1 brute force
+# Approach 1 brute force...
+# Hmm but in leet code still got 100% time and space efficency? 
 def shortest_palindrome(s)
+    s_rev = s.reverse
+    s_size = s.length
+
+    i = 0
+    while i < s_size 
+        return s if s == s_rev
+        return s_rev[0..i - 1] + s if s_rev[i..] == s[0..(s_size - 1) - i]
+        i += 1
+    end 
+    return ''
     
 end
 
@@ -25,8 +43,6 @@ end
 
 # Return the shortest palindrome you can find by performing this transformation.
 
- 
-
 # Example 1:
 
 # Input: s = "aacecaaa"
@@ -41,3 +57,7 @@ end
 
 # 0 <= s.length <= 5 * 104
 # s consists of lowercase English letters only.
+
+# Testing 
+# s = "abcbabcab"
+# puts  shortest_palindrome(s)
