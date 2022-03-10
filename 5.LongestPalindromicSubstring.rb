e526c9baba9492bd28677339a3b86fdb59d6910a
bv@bartosarchitecture.com <bv@bartosarchitecture.com>
2022-03-10 15:52:53 -0800

    New problem of the day.

---
+++ 5.LongestPalindromicSubstring.rb
@@ -0,0 +1,42 @@
+# 5. Longest Palindromic Substring.rb
+
+=begin
+    Problem:
+        Given a string s, return the longest palindromic substring in s.
+    Time:
+    Space:
+    Notes:
+=end
+
+# @param {String} s
+# @return {String}
+def longest_palindrome(s)
+    
+end
+
+=begin
+Process:
+    Problem:
+        Given a string s, return the longest palindromic substring in s.
+        
+        inputs:
+            a string s 
+            has a lenght of length 1 to 1000
+            s can have digits 0-9 and english characters a-z
+
+        outputs:
+            longest substring that is a palindrome
+
+            examples:
+            1.  inputs: s = "babad"
+                output: "bad" or "aba"
+            
+            2.  inputs: s = "cbbd"
+                output: "bb"
+        
+        notes:
+
+    Algorithm:
+    Solve:
+    Refine:
+=end
